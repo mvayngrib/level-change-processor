49aecc8f46298d797987bdf5feccc10e1886381d
Mark Vayngrib <mark@tradle.io>
2016-05-16 12:15:55 -0400

    add 'live' event for when processor is caught up to feed

--- index.js
+++ index.js
@@ -11,38 +11,62 @@ function Processor(opts) {
   var db = this.db = this.options.db
   var worker = this.worker = this.options.worker
   var feed = this.feed = this.options.feed
-  
+
   this.key = opts.key || 'latest'
   this.dbOptions = opts.dbOptions || {valueEncoding: 'utf8'}
-  
+
+  var count
+  feed.count(function (err, n) {
+    if (err) self.emit('error', new Error('feed failed to initialize'))
+
+    count = n
+    start()
+  })
+
+  this.parsedLatest
   db.get(this.key, this.dbOptions, function(err, latest) {
     if (self.destroyed) return
     if (err && !err.notFound) return self.emit('error', err)
-      
-    var parsedLatest
+
     if (err) parsedLatest = 0
     else parsedLatest = parseInt(latest)
-      
+
     if (isNaN(parsedLatest)) return self.emit('error', new Error('corrupted latest: ' + latest))
-    
+
+    self.parsedLatest = parsedLatest
+    start()
+  })
+
+  function start () {
+    if (!(typeof count === 'number' && typeof self.parsedLatest === 'number')) return
+
+    self._checkLive()
     self.feedReadStream = feed.createReadStream({since: parsedLatest, live: true})
     self.feedReadStream.pipe(self)
     self.feedReadStream.on('error', function(err) {
       self.emit('error', err)
     })
-    
-    self.emit('processing', parsedLatest)
-  })
-  
+
+    self.emit('processing', self.parsedLatest)
+  }
+
   Writable.call(this, {objectMode: true, highWaterMark: 16})
 }
 
 inherits(Processor, Writable)
 
+Processor.prototype._checkLive = function (parsedLatest) {
+  if (this.parsedLatest === this.feed.change + (this.feed.queued || 0)) {
+    this.emit('live')
+  }
+}
+
 Processor.prototype._write = function(obj, enc, cb) {
   var self = this
   this.worker(obj, function(err) {
     if (err) return cb(err)
+    self.parsedLatest = obj.change
+    self._checkLive()
     self.db.put(self.key, obj.change.toString(), self.dbOptions, function(err) {
       if (err) return cb(err)
       cb()
--- test.js
+++ test.js
@@ -5,49 +5,49 @@ var changeProcessor = require('./')
 
 test('tails the feed', function(t) {
   t.plan(3)
-  
+
   var feedDb = memdb()
   var stateDb = memdb()
-  
+
   var feed = changesFeed(feedDb)
-  
+
   var processor = changeProcessor({
     db: stateDb,
     feed: feed,
     worker: worker
   })
-    
+
   function worker(change, cb) {
     t.ok(change, 'got change')
     t.ok(change.value, 'change.value')
     t.ok(change.change, 'change.change')
     cb()
   }
-  
+
   feed.append('hello')
 })
 
 test('restarts from where it left off', function(t) {
   t.plan(7)
-  
+
   var feedDb = memdb()
   var stateDb = memdb()
-  
+
   var feed = changesFeed(feedDb)
-  
+
   var processor = changeProcessor({
     db: stateDb,
     feed: feed,
     worker: worker
   })
-    
+
   var counter = 0
-  
+
   function worker(change, cb) {
     counter++
     t.ok(change, 'got change')
     t.ok(change.change < 3, 'change is < 3')
-    
+
     if (counter === 2) {
       processor.destroy()
       setTimeout(function() {
@@ -56,7 +56,7 @@ test('restarts from where it left off', function(t) {
           feed: feed,
           worker: resumedWorker
         })
-        
+
         function resumedWorker(change, cb) {
           t.ok(change, 'got change after resume')
           t.equals(change.value.toString(), 'hello3', 'got hello3')
@@ -65,36 +65,63 @@ test('restarts from where it left off', function(t) {
         }
       }, 500)
     }
-    
+
     cb()
   }
-  
+
   feed.append('hello1')
   feed.append('hello2')
   feed.append('hello3')
 })
 
-test('handling errors', function(t) {  
+test('handling errors', function(t) {
   var feedDb = memdb()
   var stateDb = memdb()
-  
+
   var feed = changesFeed(feedDb)
-  
+
   var processor = changeProcessor({
     db: stateDb,
     feed: feed,
     worker: worker
   })
-    
+
   function worker(change, cb) {}
-  
+
   processor.on('error', function(err) {
     t.ok(err, 'got err')
     t.equals(err.message, 'oh god')
     t.end()
   })
-  
+
   processor.on('processing', function(latest) {
     processor.feedReadStream.destroy(new Error('oh god'))
+  })
+})
+
+test('live', function (t) {
+  t.plan(1)
+
+  var feedDb = memdb()
+  var stateDb = memdb()
+
+  var feed = changesFeed(feedDb)
+
+  var processed = 0
+  var processor = changeProcessor({
+    db: stateDb,
+    feed: feed,
+    worker: worker
+  })
+
+  function worker(change, cb) {
+    processed++
+    if (processed === 1) feed.append('world')
+    cb()
+  }
+
+  feed.append('hello')
+  processor.on('live', function () {
+    t.equal(processed, 2)
   })
 })
